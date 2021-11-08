189f141f1ee23f7a83dbd2693f90a455172601a8
Victor Ionescu <dsign.victor@gmail.com>
2021-11-08 18:08:21 +0200

    switch to Lua

---
+++ init.lua
@@ -0,0 +1,273 @@
+-- TODO:
+-- - fix lsp icons
+-- - clean up commented stuff
+-- - Trouble open/close
+-- - Trouble/lsp side flicker
+-- - lsp keys
+-- - research Neorg
+-- - LSP servers installer only one array
+-- - null-ls?
+
+-------------------- HELPERS -------------------------------
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn    -- to callnged the default  Vim functions e.g. fn.bufnr()
+local g = vim.g      -- a table to access global variables
+local opt = vim.opt  -- to set options
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+g.mapleader=' '
+
+-------------------- PLUGINS -------------------------------
+-- Install packer
+local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+
+if fn.empty(vim.fn.glob(install_path)) > 0 then
+  fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+vim.api.nvim_exec(
+  [[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost init.lua PackerCompile
+  augroup end
+]],
+  false
+)
+
+local use = require('packer').use
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim' -- Package manager
+
+  use {'junegunn/fzf', run = function() vim.fn['fzf#install']() end}
+  use {'junegunn/fzf.vim'}
+
+  -- Commenting and complex aligning
+  use {'junegunn/vim-easy-align'}
+  use {'tpope/vim-repeat'}
+  use {'tomtom/tcomment_vim'}
+  use {'Yggdroot/indentLine'}
+
+  -- Autoclose braces and surround selection with braces...
+  use {'cohama/lexima.vim'}
+  use {'tpope/vim-surround'}
+  use {'andymass/vim-matchup'}
+
+  -- Theme
+  use {'morhetz/gruvbox'}
+
+  -- Statusline
+  use {'hoob3rt/lualine.nvim'}
+  use {'kyazdani42/nvim-web-devicons'}
+  use {'ap/vim-buftabline'}
+  use {'ojroques/nvim-bufdel'}
+
+  -- Autocompletion
+  use {'neovim/nvim-lspconfig'}
+  use {'williamboman/nvim-lsp-installer'}
+  use {'glepnir/lspsaga.nvim'}
+  -- use 'hrsh7th/cmp-nvim-lsp'
+  -- use 'hrsh7th/cmp-buffer'
+  -- use 'hrsh7th/cmp-path'
+  -- use 'hrsh7th/cmp-cmdline'
+  -- use 'hrsh7th/nvim-cmp'
+
+  -- use 'hrsh7th/cmp-vsnip'
+  -- use 'hrsh7th/vim-vsnip'
+  -- use 'L3MON4D3/LuaSnip'
+  -- use 'saadparwaiz1/cmp_luasnip'
+  --
+  -- use 'jose-elias-alvarez/null-ls.nvim'
+
+
+  --- Lsp
+  use({
+      -- 'neovim/nvim-lspconfig',
+      'ray-x/lsp_signature.nvim',
+      'jose-elias-alvarez/nvim-lsp-ts-utils',
+  })
+
+  use({
+      'jose-elias-alvarez/null-ls.nvim',
+      requires = {
+          'nvim-lua/plenary.nvim',
+          'neovim/nvim-lspconfig',
+      },
+  })
+
+  -- Completion
+  use({
+      'hrsh7th/nvim-cmp',
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lua',
+      'lukas-reineke/cmp-rg',
+  })
+  use 'creativenull/diagnosticls-configs-nvim'
+
+  -- Snippets
+  use({
+      'L3MON4D3/luasnip',
+      requires = {
+          'rafamadriz/friendly-snippets',
+      },
+  })
+
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  use {'ojroques/nvim-lspfuzzy'}
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup()
+    end
+  }
+
+  -- Syntax
+  use {
+    'norcalli/nvim-colorizer.lua',
+    ft = { 'css', 'javascript', 'vim', 'html' },
+    config = [[require('colorizer').setup {'css', 'javascript', 'vim', 'html'}]],
+  }
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function () require('gitsigns').setup() end
+  }
+
+  -- Dev helpers (linting, project spacing...)
+  use {'editorconfig/editorconfig-vim'}
+  use {'mattn/emmet-vim'}
+  use {
+    'yardnsm/vim-import-cost',
+    run = 'npm install',
+    ft = {'javascript', 'javascript.jsx','typescript'}
+  }
+  use {
+    'heavenshell/vim-jsdoc',
+    run = 'make install',
+    ft = {'javascript', 'javascript.jsx','typescript'}
+  }
+  use {
+    'prettier/vim-prettier',
+    run = 'npm install',
+    ft = {'javascript', 'typescript', 'css', 'less', 'scss', 'json', 'graphql', 'markdown', 'vue', 'svelte', 'yaml', 'html'}
+  }
+
+end)
+-------------------- OPTIONS -------------------------------
+cmd 'colorscheme gruvbox'            -- Put your favorite colorscheme here
+opt.completeopt = {'menuone', 'noinsert', 'noselect'}  -- Completion options (for deoplete)
+opt.colorcolumn = {80}
+opt.expandtab = true                -- Use spaces instead of tabs
+opt.number = true                   -- Show line numbers
+opt.relativenumber = true           -- Relative line numbers
+opt.fileformat = 'unix'
+opt.mouse = 'a'
+opt.shiftwidth = 2                  -- Size of an indent
+opt.tabstop = 2                     -- Number of spaces tabs count for
+opt.termguicolors = true            -- True color support
+opt.wrap = true
+-- opt.scrolloff = 4                   -- Lines of context
+-- opt.shiftround = true               -- Round indent
+-- opt.sidescrolloff = 8               -- Columns of context
+-- opt.smartcase = true                -- Do not ignore case with capitals
+opt.smartindent = true              -- Insert indents automatically
+-- opt.splitbelow = true               -- Put new windows below current
+-- opt.splitright = true               -- Put new windows right of current
+-- opt.wildmode = {'list', 'longest'}  -- Command-line completion mode
+-- opt.hidden = true                  -- Enable background buffers
+-- opt.ignorecase = true               -- Ignore case
+-- opt.joinspaces = false              -- No double spaces with join
+-- opt.list = true                     -- Show some invisible characters
+
+-------------------- MAPPINGS ------------------------------
+-- https://github.com/junegunn/fzf/issues/337
+cmd [[let $FZF_DEFAULT_COMMAND = 'ag --hidden --ignore .git -l -g ""']]
+
+map('v', '<leader>y', '"+y')       -- Copy to clipboard in visual modes
+map('n', '<leader>p', '"+p')       -- Copy to clipboard in visual modes
+
+map('i', '<C-w>', '<C-g>u<C-w>')  -- Make <C-w> undo-friendly
+-- <Tab> to navigate the completion menu
+map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
+map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
+
+map('n', '<C-l>', '<cmd>noh<CR>')    -- Clear highlights
+map('c', 'W', 'w')
+map('', '<leader>o', ':Files <CR>')
+map('', '<leader>b', ':Buffers <CR>')
+map('n', '<leader>s', ':source ~/.config/nvim/init.lua<CR>')
+map('', '<leader>t', ':TroubleToggle<CR>')
+cmd [[
+:cnoreabbrev wq w<bar>BufDel
+:cnoreabbrev q BufDel
+:cnoreabbrev Q q
+]]
+
+-------------------- TREE-SITTER ---------------------------
+local ts = require 'nvim-treesitter.configs'
+ts.setup {
+  ensure_installed = 'all',
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = true
+  },
+  indent = {
+    enable = false
+  },
+  context_commentstring = {
+    enable = true
+  },
+  matchup = {
+    enable = true,              -- mandatory, false will disable the whole extension
+  }
+}
+
+-------------------- LSP -----------------------------------
+require('lsp')
+------------------------------------------------------------
+
+require'lualine'.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'gruvbox',
+    component_separators = {'', ''},
+    section_separators = {'', ''},
+    disabled_filetypes = {}
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
+
+-------------------- COMMANDS ------------------------------
+vim.api.nvim_set_var('jsdoc_formatter', 'tsdoc')
+cmd 'au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}'
+
+-- don't auto commenting new lines
+cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
+-- disable IndentLine for markdown files (avoid concealing)
+cmd [[autocmd FileType markdown let g:indentLine_enabled=0]]
---
+++ lua/lsp.lua
@@ -0,0 +1,239 @@
+local cmd = vim.cmd
+local nvim_lsp = require 'lspconfig'
+-- npm install -g diagnostic-languageserver eslint_d prettier_d_slim prettier
+local function on_attach(client)
+  print('Attached to ' .. client.name)
+end
+local dlsconfig = require 'diagnosticls-configs'
+dlsconfig.init {
+  default_config = false,
+  format = true,
+  on_attach = on_attach,
+}
+local eslint = require 'diagnosticls-configs.linters.eslint'
+local prettier = require 'diagnosticls-configs.formatters.prettier'
+prettier.requiredFiles = {
+    '.prettierrc',
+    '.prettierrc.json',
+    '.prettierrc.toml',
+    '.prettierrc.json',
+    '.prettierrc.yml',
+    '.prettierrc.yaml',
+    '.prettierrc.json5',
+    '.prettierrc.js',
+    '.prettierrc.cjs',
+    'prettier.config.js',
+    'prettier.config.cjs',
+  }
+dlsconfig.setup {
+  ['javascript'] = {
+    linter = eslint,
+    formatter = { prettier }
+  },
+  ['javascriptreact'] = {
+    linter = { eslint },
+    formatter = { prettier }
+  },
+  ['css'] = {
+    formatter = prettier
+  },
+}
+local lsp_symbols = {
+  Text = "   (Text) ",
+  Method = "   (Method)",
+  Function = "   (Function)",
+  Constructor = "   (Constructor)",
+  Field = " ﴲ  (Field)",
+  Variable = "[] (Variable)",
+  Class = "   (Class)",
+  Interface = " ﰮ  (Interface)",
+  Module = "   (Module)",
+  Property = " 襁 (Property)",
+  Unit = "   (Unit)",
+  Value = "   (Value)",
+  Enum = " 練 (Enum)",
+  Keyword = "   (Keyword)",
+  Snippet = "   (Snippet)",
+  Color = "   (Color)",
+  File = "   (File)",
+  Reference = "   (Reference)",
+  Folder = "   (Folder)",
+  EnumMember = "   (EnumMember)",
+  Constant = " ﲀ  (Constant)",
+  Struct = " ﳤ  (Struct)",
+  Event = "   (Event)",
+  Operator = "   (Operator)",
+  TypeParameter = "   (TypeParameter)",
+  -- '', -- Text
+  -- '', -- Method
+  -- '', -- Function
+  -- '', -- Constructor
+  -- '', -- Field
+  -- '', -- Variable
+  -- '', -- Class
+  -- 'ﰮ', -- Interface
+  -- '', -- Module
+  -- '', -- Property
+  -- '', -- Unit
+  -- '', -- Value
+  -- '', -- Enum
+  -- '', -- Keyword
+  -- '﬌', -- Snippet
+  -- '', -- Color
+  -- '', -- File
+  -- '', -- Reference
+  -- '', -- Folder
+  -- '', -- EnumMember
+  -- '', -- Constant
+  -- '', -- Struct
+  -- '', -- Event
+  -- 'ﬦ', -- Operator
+  -- '', -- TypeParameter
+}
+
+cmd[[
+nnoremap <silent> gd    <cmd>lua vim.lsp.buf.definition()<CR>
+nnoremap <silent> gh    <cmd>lua vim.lsp.buf.hover()<CR>
+nnoremap <silent> gca   <cmd>:Telescope lsp_code_actions<CR>
+nnoremap <silent> gD    <cmd>lua vim.lsp.buf.implementation()<CR>
+nnoremap <silent> <c-k> <cmd>lua vim.lsp.buf.signature_help()<CR>
+nnoremap <silent> gr    <cmd>lua vim.lsp.buf.references()<CR>
+nnoremap <silent> gR    <cmd>lua vim.lsp.buf.rename()<CR>
+nnoremap <silent><leader>fo <cmd>lua vim.lsp.buf.formatting()<CR>
+]]
+
+local lsp_installer = require("nvim-lsp-installer")
+
+lsp_installer.on_server_ready(function(server)
+    local opts = {
+      'bashls',
+      'diagnosticls',
+      'cssls',
+      'eslint',
+      'elmls',
+      'emmet_ls',
+      'graphql',
+      'html',
+      'jsonls',
+      'tailwindcss',
+      'tsserver',
+      'vimls',
+      'sumneko_lua'
+  }
+    --
+    server:setup(opts)
+
+    vim.cmd [[ do User LspAttachBuffers ]]
+
+end)
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- Enable the following language servers
+local servers = {
+  'bashls',
+  'diagnosticls',
+  'cssls',
+  'eslint',
+  'elmls',
+  'emmet_ls',
+  'graphql',
+  'html',
+  'jsonls',
+  'tailwindcss',
+  'tsserver',
+  'vimls',
+}
+
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+nvim_lsp['sumneko_lua'].setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' },
+      },
+    },
+  },
+}
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+local luasnip = require('luasnip')
+
+cmp.setup({
+  auto_select = false,
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ["<cr>"] = cmp.mapping.confirm({select = true}),
+    ["<s-tab>"] = cmp.mapping.select_prev_item(),
+    ["<tab>"] = cmp.mapping.select_next_item(),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    -- For vsnip user.
+    -- { name = 'vsnip' },
+    -- For luasnip user.
+    { name = 'path' },
+    -- For ultisnips user.
+    -- { name = 'ultisnips' },
+    { name = 'luasnip' },
+    { name = 'buffer', keywork_length = 5 },
+    { name = 'npm', keyword_length = 4 },
+  },
+  formatting = {
+    format = function(entry, item)
+      item.kind = lsp_symbols[item.kind]
+      item.menu = ({
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        -- neorg = "[Neorg]",
+      })[entry.source.name]
+
+      return item
+    end,
+    -- format = require('lspkind').cmp_format {
+    --   with_text = true,
+    --   menu = {
+    --     buffer = "[buf]",
+    --     nvim_lsp = "[LSP]",
+    --     path = "[path]",
+    --     luasnip = "[snip]"
+    --   }
+    -- }
+  },
+  experimental = {
+    native_menu = false,
+    ghost_text = true
+  }
+})
+
+-- icon
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    -- This sets the spacing and the prefix, obviously.
+    virtual_text = false
+  }
+)
+
+local saga = require 'lspsaga'
+saga.init_lsp_saga {
+  error_sign = '',
+  warn_sign = '',
+  hint_sign = '',
+  infor_sign = '',
+  border_style = "round",
+}
