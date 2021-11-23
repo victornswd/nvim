2a7802575516c59b9643594866acb512d6e29322
Victor Ionescu <dsign.victor@gmail.com>
2021-11-23 18:36:50 +0200

    fix weird lsp flicker

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -140,11 +140,6 @@ nvim_lsp['sumneko_lua'].setup {
   },
 }
 -- Setup nvim-cmp.
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local cmp = require'cmp'
 local luasnip = require('luasnip')
 require('luasnip.loaders.from_vscode').lazy_load()
@@ -158,29 +153,8 @@ cmp.setup({
   },
   mapping = {
     ["<cr>"] = cmp.mapping.confirm({select = true}),
-    -- ["<s-tab>"] = cmp.mapping.select_prev_item(),
-    -- ["<tab>"] = cmp.mapping.select_next_item(),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+    ["<s-tab>"] = cmp.mapping.select_prev_item(),
+    ["<tab>"] = cmp.mapping.select_next_item(),
   },
   sources = {
     { name = 'nvim_lsp' },
