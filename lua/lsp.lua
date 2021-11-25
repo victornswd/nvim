40beb7dbd1af3b1300de5af14ce543e023930ed3
Victor Ionescu <dsign.victor@gmail.com>
2021-11-25 17:34:58 +0200

    show colored icons for LSP error/warnings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -204,3 +204,9 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     virtual_text = false
   }
 )
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
