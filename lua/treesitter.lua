d22f6ba1eb779a756c60767e1d4efadd0a0903e2
Victor Ionescu <dsign.victor@gmail.com>
2021-12-07 19:11:34 +0200

    move treesitter to a different file

---
+++ lua/treesitter.lua
@@ -0,0 +1,23 @@
+require 'nvim-treesitter.install'.compilers = { "gcc" }
+local ts = require 'nvim-treesitter.configs'
+ts.setup {
+  ensure_installed = 'all',
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = true
+  },
+  indent = {
+    enable = false
+  },
+  context_commentstring = {
+    enable = true
+  },
+  matchup = {
+    enable = true,   -- mandatory, false will disable the whole extension
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+}
