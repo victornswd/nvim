e5721f9b8d15ce529fb53f90c88f4a122a218c97
Victor Ionescu <dsign.victor@gmail.com>
2021-12-08 17:49:26 +0200

    removed markdown from tailwind checks

--- lua/lsp/servers/tailwind.lua
+++ lua/lsp/servers/tailwind.lua
@@ -22,7 +22,7 @@ require'lspconfig'.tailwindcss.setup {
     end
   end,
   cmd = { 'tailwindcss-language-server', '--stdio' },
-  filetypes = { "aspnetcorerazor", "astro", "astro-markdown", "blade", "django-html", "edge", "eelixir", "ejs", "erb", "eruby", "gohtml", "haml", "handlebars", "hbs", "html", "html-eex", "jade", "leaf", "liquid", "markdown", "mdx", "mustache", "njk", "nunjucks", "php", "razor", "slim", "twig", "css", "less", "postcss", "sass", "scss", "stylus", "sugarss", "javascript", "javascriptreact", "reason", "rescript", "typescript", "typescriptreact", "vue", "svelte" },
+  filetypes = { "aspnetcorerazor", "astro", "astro-markdown", "blade", "django-html", "edge", "eelixir", "ejs", "erb", "eruby", "gohtml", "haml", "handlebars", "hbs", "html", "html-eex", "jade", "leaf", "liquid", "mdx", "mustache", "njk", "nunjucks", "php", "razor", "slim", "twig", "css", "less", "postcss", "sass", "scss", "stylus", "sugarss", "javascript", "javascriptreact", "reason", "rescript", "typescript", "typescriptreact", "vue", "svelte" },
   init_options = {
     userLanguages = {
       eelixir = 'html-eex',
