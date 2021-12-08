40701dd50d4022e12c3d5cc343ab115419d9ea5e
Victor Ionescu <dsign.victor@gmail.com>
2021-12-08 13:48:09 +0200

    autoinstall TS & Tailwind LSPs + some quote cleaning

--- lua/lsp/servers/tailwind.lua
+++ lua/lsp/servers/tailwind.lua
@@ -1,5 +1,15 @@
 -- npm install -g @tailwindcss/language-server
 
+-- Auto-install
+local lsp_installer_servers = require'nvim-lsp-installer.servers'
+
+local ok, tailwindcss = lsp_installer_servers.get_server('tailwindcss')
+if ok then
+  if not tailwindcss:is_installed() then
+    tailwindcss:install()
+  end
+end
+
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.colorProvider = { dynamicRegistration = false }
@@ -8,15 +18,15 @@ require'lspconfig'.tailwindcss.setup {
   capabilities = capabilities,
   on_attach = function (client, bufnr)
     if client.server_capabilities.colorProvider then
-        require"lsp/documentcolors".buf_attach(bufnr)
+        require'lsp/documentcolors'.buf_attach(bufnr)
     end
   end,
-  cmd = { "tailwindcss-language-server", "--stdio" },
+  cmd = { 'tailwindcss-language-server', '--stdio' },
   filetypes = { "aspnetcorerazor", "astro", "astro-markdown", "blade", "django-html", "edge", "eelixir", "ejs", "erb", "eruby", "gohtml", "haml", "handlebars", "hbs", "html", "html-eex", "jade", "leaf", "liquid", "markdown", "mdx", "mustache", "njk", "nunjucks", "php", "razor", "slim", "twig", "css", "less", "postcss", "sass", "scss", "stylus", "sugarss", "javascript", "javascriptreact", "reason", "rescript", "typescript", "typescriptreact", "vue", "svelte" },
   init_options = {
     userLanguages = {
-      eelixir = "html-eex",
-      eruby = "erb"
+      eelixir = 'html-eex',
+      eruby = 'erb'
     }
   },
   on_new_config = function(new_config)
@@ -34,13 +44,13 @@ require'lspconfig'.tailwindcss.setup {
   settings = {
     tailwindCSS = {
       lint = {
-        cssConflict = "warning",
-        invalidApply = "error",
-        invalidConfigPath = "error",
-        invalidScreen = "error",
-        invalidTailwindDirective = "error",
-        invalidVariant = "error",
-        recommendedVariantOrder = "warning"
+        cssConflict = 'warning',
+        invalidApply = 'error',
+        invalidConfigPath = 'error',
+        invalidScreen = 'error',
+        invalidTailwindDirective = 'error',
+        invalidVariant = 'error',
+        recommendedVariantOrder = 'warning'
       },
       experimental = {
         classRegex = {
--- lua/lsp/servers/tsserver.lua
+++ lua/lsp/servers/tsserver.lua
@@ -14,6 +14,16 @@ capabilities.textDocument.completion.completionItem.resolveSupport = {
   }
 }
 
+-- Auto-install
+local lsp_installer_servers = require'nvim-lsp-installer.servers'
+
+local ok, tsserver = lsp_installer_servers.get_server('tsserver')
+if ok then
+  if not tsserver:is_installed() then
+    tsserver:install()
+  end
+end
+
 -- npm install -g typescript typescript-language-server
 require'lspconfig'.tsserver.setup({
   capabilities = capabilities,
@@ -22,46 +32,46 @@ require'lspconfig'.tsserver.setup({
     client.resolved_capabilities.document_range_formatting = false
 
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-    require("nvim-lsp-ts-utils").setup {
-        debug = false,
-        disable_commands = false,
-        enable_import_on_completion = true,
-        import_all_timeout = 5000, -- ms
+    require('nvim-lsp-ts-utils').setup {
+      debug = false,
+      disable_commands = false,
+      enable_import_on_completion = true,
+      import_all_timeout = 5000, -- ms
 
-        -- eslint
-        eslint_enable_code_actions = false,
-        eslint_enable_disable_comments = false,
-        eslint_bin = 'eslint_d',
-        eslint_config_fallback = nil,
-        eslint_enable_diagnostics = false,
-        eslint_opts = {
-          -- diagnostics_format = "#{m} [#{c}]",
-          condition = function(utils)
-              return utils.root_has_file(".eslintrc.js")
-          end,
-        },
+      -- eslint
+      eslint_enable_code_actions = false,
+      eslint_enable_disable_comments = false,
+      eslint_bin = 'eslint_d',
+      eslint_config_fallback = nil,
+      eslint_enable_diagnostics = false,
+      eslint_opts = {
+        -- diagnostics_format = "#{m} [#{c}]",
+        condition = function(utils)
+          return utils.root_has_file(".eslintrc.js")
+        end,
+      },
 
-        -- formatting
-        enable_formatting = false,
-        formatter = 'prettier_d_slim',
-        formatter_config_fallback = nil,
+      -- formatting
+      enable_formatting = false,
+      formatter = 'prettier_d_slim',
+      formatter_config_fallback = nil,
 
-        -- parentheses completion
-        complete_parens = false,
-        signature_help_in_parens = false,
+      -- parentheses completion
+      complete_parens = false,
+      signature_help_in_parens = false,
 
-        -- update imports on file move
-        update_imports_on_move = true,
-        require_confirmation_on_move = true,
-        watch_dir = nil,
+      -- update imports on file move
+      update_imports_on_move = true,
+      require_confirmation_on_move = true,
+      watch_dir = nil,
 
-        -- filter diagnostics
-        filter_out_diagnostics_by_severity = { "hint" },
-        filter_out_diagnostics_by_code = {},
+      -- filter diagnostics
+      filter_out_diagnostics_by_severity = { 'hint' },
+      filter_out_diagnostics_by_code = {},
     }
 
-    require("nvim-lsp-ts-utils").setup_client(client)
+    require('nvim-lsp-ts-utils').setup_client(client)
   end
 })
