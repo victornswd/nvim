1cb36900d315b1217114c4cd1dc388535683e4fb
Victor Ionescu <dsign.victor@gmail.com>
2021-12-08 17:50:36 +0200

    dimm the indentlines

--- init.lua
+++ init.lua
@@ -51,15 +51,7 @@ require('packer').startup(function()
     })
     end
   }
-  use {'lukas-reineke/indent-blankline.nvim',
-    config = function ()
-     require("indent_blankline").setup {
-       space_char_blankline = " ",
-       show_current_context = true,
-       show_current_context_start = true,
-     }
-    end
-  }
+  use {'lukas-reineke/indent-blankline.nvim'}
 
   -- Autoclose braces and surround selection with braces...
   use {'cohama/lexima.vim'}
@@ -171,8 +163,19 @@ end)
 -------------------- OPTIONS -------------------------------
 -- cmd 'colorscheme gruvbox'
 vim.g.rose_pine_variant = 'moon'
+
 -- Load colorscheme after options
 vim.cmd('colorscheme rose-pine')
+
+-- set the colors manually from the rose-pine theme
+vim.cmd [[highlight IndentBlanklineChar guifg=#59546d gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineContextChar guifg=#c4a7e7 gui=nocombine]]
+require("indent_blankline").setup {
+  space_char_blankline = " ",
+  show_current_context = true,
+  show_current_context_start = true,
+  indent_blankline_use_treesitter = true,
+}
 
 opt.completeopt = {'menuone', 'noinsert', 'noselect'}  -- Completion options (for deoplete)
 opt.colorcolumn = {80}
