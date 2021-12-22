e684b4e851c3bad30b0d49970374613b80247f5a
Victor Ionescu <dsign.victor@gmail.com>
2021-12-22 19:58:36 +0200

    disable some built-in nvim functions for faster start-up

--- init.lua
+++ init.lua
@@ -33,6 +33,28 @@ cmd [[command! PackerCompile packadd packer.nvim | lua require('plugins').compil
 require('colors')
 
 -------------------- OPTIONS -------------------------------
+g.loaded_gzip = 1
+g.loaded_zip = 1
+g.loaded_zipPlugin = 1
+g.loaded_tar = 1
+g.loaded_tarPlugin = 1
+
+g.loaded_getscript = 1
+g.loaded_getscriptPlugin = 1
+g.loaded_vimball = 1
+g.loaded_vimballPlugin = 1
+g.loaded_2html_plugin = 1
+
+g.loaded_matchit = 1
+g.loaded_matchparen = 1
+g.loaded_logiPat = 1
+g.loaded_rrhelper = 1
+
+-- g.loaded_netrw = 1
+-- g.loaded_netrwPlugin = 1
+-- g.loaded_netrwSettings = 1
+-- g.loaded_netrwFileHandlers = 1
+
 opt.completeopt = {'menuone', 'noinsert', 'noselect'}
 opt.colorcolumn = {80}
 opt.expandtab = true                -- Use spaces instead of tabs
