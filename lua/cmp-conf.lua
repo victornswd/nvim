baf25a4fef88ac0a36738742ab5f155193992589
Victor Ionescu <dsign.victor@gmail.com>
2021-12-23 19:38:35 +0200

    revert to deprecated `unpack`

--- lua/cmp-conf.lua
+++ lua/cmp-conf.lua
@@ -51,7 +51,7 @@ require('luasnip.loaders.from_vscode').load({include = {'markdown'}})
 require('luasnip.loaders.from_vscode').lazy_load()
 
 local has_words_before = function()
-  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
