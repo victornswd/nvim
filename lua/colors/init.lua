8f49bb2b16c44f5b9a16ab5a9fe2113c574fff65
Victor Ionescu <dsign.victor@gmail.com>
2022-02-25 20:51:36 +0200

    added commented colors for mini.base16

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -16,3 +16,24 @@ end
 -- -- Load colorscheme after options
 -- vim.cmd('colorscheme rose-pine')
 
+-- require('mini.base16').setup({
+--   palette = {
+--     base00 = '#2b3339',
+--     base01 = '#363e44',
+--     base02 = '#4a5258',
+--     base03 = '#5e666c',
+--     base04 = '#e69875',
+--     base05 = '#d1b171',
+--     base06 = '#dbbc7f',
+--     base07 = '#b4bbc8',
+--     base08 = '#D3C6AA',
+--     base09 = '#cc7e46',
+--     base0A = '#83c092',
+--     base0B = '#a7c080',
+--     base0C = '#ff75a0',
+--     base0D = '#69a59d',
+--     base0E = '#d699b6',
+--     base0F = '#95d1c9',
+--   },
+--   use_cterm = true,
+-- })
