d6932150b662d77021d5264801cfe45878f76e58
Victor Ionescu <dsign.victor@gmail.com>
2022-03-16 12:52:06 +0200

    use the colorscheme global to set colors

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -1,5 +1,11 @@
 -- set the colors manually
-local colors = require('hl_themes.everforest')
+local i, j = string.find(cs, '-base16')
+local th = cs
+if i then
+  th = string.sub(cs, 1, (i - 1))
+end
+local colors = require('hl_themes.' .. th)
+
 local fg = function(group, col)
   vim.cmd('hi ' .. group .. ' guifg=' .. col)
 end
