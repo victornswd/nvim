fd3e4ccb78b07d391000e24228b71a01790e8e2d
Victor Ionescu <dsign.victor@gmail.com>
2022-04-25 16:36:37 +0300

    wrap mappings into pcall() for error free loading

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,7 +2,7 @@ _G.project_files = function()
   local opts = {}
   local ok = pcall(require('telescope.builtin').git_files, opts)
   if not ok then
-    require('telescope.builtin').find_files(opts)
+    pcall(require('telescope.builtin').find_files, opts)
   end
 end
 
@@ -36,12 +36,20 @@ vim.cmd([[
 
 -- KEYS
 -- LEADER
-vim.keymap.set('n', '<leader>f', require('telescope.builtin').live_grep, { desc = 'Search for word in folder' })
-vim.keymap.set('n', '<leader>h', require('telescope.builtin').keymaps, { desc = 'Show key maps' })
-vim.keymap.set('n', '<leader>c', require('telescope').extensions.themes.themes, { desc = 'Colorschemes' })
-vim.keymap.set('n', '<leader>d', require('neogen').generate, { desc = 'Generate function documentation' })
+vim.keymap.set('n', '<leader>f', function()
+  pcall(require('telescope.builtin').live_grep)
+end, { desc = 'Search for word in folder' })
+vim.keymap.set('n', '<leader>h', function()
+  pcall(require('telescope.builtin').keymaps)
+end, { desc = 'Show key maps' })
+vim.keymap.set('n', '<leader>c', function()
+  pcall(require('telescope').extensions.themes.themes)
+end, { desc = 'Colorschemes' })
+vim.keymap.set('n', '<leader>d', function()
+  pcall(require('neogen').generate)
+end, { desc = 'Generate function documentation' })
 vim.keymap.set('n', '<leader>t', function()
-  require('telescope.builtin').diagnostics({ bufnr = 0 })
+  pcall(require('telescope.builtin').diagnostics(), { bufnr = 0 })
 end, { desc = 'Show file diagnostics' })
 vim.keymap.set('n', '<leader>o', project_files, { desc = 'Open file search' })
 vim.keymap.set('n', '<leader>fo', vim.lsp.buf.formatting, { desc = 'Format buffer with LSP' })
@@ -56,8 +64,12 @@ vim.keymap.set('n', 'gh', vim.lsp.buf.hover, { desc = 'Display hover tooltip' })
 vim.keymap.set('n', 'gD', vim.lsp.buf.implementation, { desc = 'Go to implementation' })
 vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { desc = 'Go to type definition' })
 vim.keymap.set('n', 'gR', vim.lsp.buf.rename, { desc = 'Rename all references' })
-vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = 'References' })
-vim.keymap.set('n', 'gca', require('telescope.builtin').lsp_code_actions, { desc = 'Code actions' })
+vim.keymap.set('n', 'gr', function()
+  pcall(require('telescope.builtin').lsp_references)
+end, { desc = 'References' })
+vim.keymap.set('n', 'gca', function()
+  pcall(require('telescope.builtin').lsp_code_actions)
+end, { desc = 'Code actions' })
 
 -- CTRL
 vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { desc = 'Function signature help' })
@@ -70,7 +82,10 @@ vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move down selected line' 
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move up selected line' })
 
 -- HOP
-vim.keymap.set('n', 'h', require('hop').hint_words, { desc = 'Hop Word' })
-vim.keymap.set('n', 'l', require('hop').hint_lines, { desc = 'Hop Line' })
-vim.keymap.set('n', 's', require('hop').hint_char1, { desc = 'Hop 1 Char' })
-vim.keymap.set('n', 'S', require('hop').hint_char2, { desc = 'Hop 2 Char' })
+local iz, hop = pcall(require, 'hop')
+if iz then
+  vim.keymap.set('n', 'h', hop.hint_words, { desc = 'Hop Word' })
+  vim.keymap.set('n', 'l', hop.hint_lines, { desc = 'Hop Line' })
+  vim.keymap.set('n', 's', hop.hint_char1, { desc = 'Hop 1 Char' })
+  vim.keymap.set('n', 'S', hop.hint_char2, { desc = 'Hop 2 Char' })
+end
