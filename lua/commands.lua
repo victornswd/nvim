01ab4f0b8301eacb282ce3a56ec036e3afeec1cd
Victor Ionescu <dsign.victor@gmail.com>
2022-04-26 16:35:10 +0300

    fix packer compile autocommand

--- lua/commands.lua
+++ lua/commands.lua
@@ -14,7 +14,7 @@ end
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd(
   'BufWritePost',
-  { command = 'source <afile> | PackerCompile', group = packer_group, pattern = 'init.lua' }
+  { command = 'source <afile> | PackerCompile', group = packer_group, pattern = 'lua/plugin-list.lua' }
 )
 
 vim.api.nvim_create_user_command(
