bb7855cab8a369e32b84c1bb35e3076c270936c6
Victor Ionescu <dsign.victor@gmail.com>
2022-07-12 18:52:51 +0300

    update jsonls & html LSP configs

--- lua/config/lsp/servers/json.lua
+++ lua/config/lsp/servers/json.lua
@@ -1,55 +1,45 @@
-local M = {}
+local lspconfig = require('lspconfig')
+local conf = require('config.lsp.helpers')
 
--- Auto-install
-
-local lsp_installer_servers = require('nvim-lsp-installer.servers')
-
-local ok, jsonls = lsp_installer_servers.get_server('jsonls')
-if ok then
-  if not jsonls:is_installed() then
-    jsonls:install()
-  end
-end
-
--- Settings
-
-M.settings = {
-  json = {
-    schemas = {
-      {
-        fileMatch = { 'package.json' },
-        url = 'https://json.schemastore.org/package.json',
-      },
-      {
-        fileMatch = { 'tsconfig*.json' },
-        url = 'https://json.schemastore.org/tsconfig.json',
-      },
-      {
-        fileMatch = { '.prettierrc', '.prettierrc.json', 'prettier.config.json' },
-        url = 'https://json.schemastore.org/prettierrc.json',
-      },
-      {
-        fileMatch = { '.eslintrc', '.eslintrc.json' },
-        url = 'https://json.schemastore.org/eslintrc.json',
-      },
-      {
-        fileMatch = { '.babelrc', '.babelrc.json', 'babel.config.json' },
-        url = 'https://json.schemastore.org/babelrc.json',
-      },
-      {
-        fileMatch = { 'lerna.json' },
-        url = 'https://json.schemastore.org/lerna.json',
-      },
-      {
-        fileMatch = { 'now.json', 'vercel.json' },
-        url = 'https://json.schemastore.org/now.json',
-      },
-      {
-        fileMatch = { 'ecosystem.json' },
-        url = 'https://json.schemastore.org/pm2-ecosystem.json',
+lspconfig['jsonls'].setup({
+  capabilities = conf.capabilities,
+  on_attach = conf.on_attach,
+  settings = {
+    json = {
+      schemas = {
+        {
+          fileMatch = { 'package.json' },
+          url = 'https://json.schemastore.org/package.json',
+        },
+        {
+          fileMatch = { 'tsconfig*.json' },
+          url = 'https://json.schemastore.org/tsconfig.json',
+        },
+        {
+          fileMatch = { '.prettierrc', '.prettierrc.json', 'prettier.config.json' },
+          url = 'https://json.schemastore.org/prettierrc.json',
+        },
+        {
+          fileMatch = { '.eslintrc', '.eslintrc.json' },
+          url = 'https://json.schemastore.org/eslintrc.json',
+        },
+        {
+          fileMatch = { '.babelrc', '.babelrc.json', 'babel.config.json' },
+          url = 'https://json.schemastore.org/babelrc.json',
+        },
+        {
+          fileMatch = { 'lerna.json' },
+          url = 'https://json.schemastore.org/lerna.json',
+        },
+        {
+          fileMatch = { 'now.json', 'vercel.json' },
+          url = 'https://json.schemastore.org/now.json',
+        },
+        {
+          fileMatch = { 'ecosystem.json' },
+          url = 'https://json.schemastore.org/pm2-ecosystem.json',
+        },
       },
     },
   },
-}
-
-return M
+})
