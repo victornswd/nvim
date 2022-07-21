8bfad97173a73b7a51addada3f1bc591cdb05e4c
Victor Ionescu <dsign.victor@gmail.com>
2022-07-21 20:49:14 +0300

    added LSP connected and loading functions

--- lua/config/status.lua
+++ lua/config/status.lua
@@ -364,6 +364,46 @@ local get_sign = function(severity)
   return icons[severity].text
 end
 
+M.lsp_loading = function()
+  local Lsp = vim.lsp.util.get_progress_messages()[1]
+
+  if Lsp then
+    local msg = Lsp.message or ''
+    local percentage = Lsp.percentage or 0
+    local title = Lsp.title or ''
+    local spinners = {
+      '',
+      '',
+      '',
+    }
+
+    local success_icon = {
+      '',
+      '',
+      '',
+    }
+
+    local ms = vim.loop.hrtime() / 1000000
+    local frame = math.floor(ms / 120) % #spinners
+
+    if percentage >= 70 then
+      return string.format(' %%<%s %s %s (%s%%%%) ', success_icon[frame + 1], title, msg, percentage)
+    end
+
+    return string.format(' %%<%s %s %s (%s%%%%) ', spinners[frame + 1], title, msg, percentage)
+  end
+
+  return ''
+end
+
+M.lsp_connected = function()
+  if next(vim.lsp.buf_get_clients()) ~= nil then
+    return ' LSP'
+  else
+    return ''
+  end
+end
+
 M.get_diag = function()
   local d = vim.diagnostic.get(0)
   if #d == 0 then
@@ -496,6 +536,8 @@ M.get_statusline = function()
     '%{%v:lua.status.get_git_removed()%}',
     -- '%#StatusLineMod#%{IsBuffersModified()}%*',
     '%=',
+    '%#StatusLineGitBranch# %{%v:lua.status.lsp_connected()%}%*',
+    '%#StatusLineGitAdd# %{%v:lua.status.lsp_loading()%}%*',
     '%{%v:lua.status.get_diag_counts()%}',
     '%#StatusLineLocationSep#%*',
     '%#StatusLineLocation# %{%v:lua.status.get_loc()%}%*',
