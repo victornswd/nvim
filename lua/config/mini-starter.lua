8f6bacef07978351c5ca75b41ee416786b757c8b
Victor Ionescu <dsign.victor@gmail.com>
2022-08-12 16:28:34 +0300

    add harpoon to starter

--- lua/config/mini-starter.lua
+++ lua/config/mini-starter.lua
@@ -28,6 +28,11 @@ local function telescope()
     { action = 'lua project_files()', name = 'Files', section = 'File Management' },
     { action = 'Telescope live_grep', name = 'Live grep', section = 'File Management' },
     { action = 'Telescope oldfiles', name = 'Old files', section = 'File Management' },
+    {
+      action = "lua pcall(require('harpoon.ui').toggle_quick_menu)",
+      name = 'List Harpoon',
+      section = 'List Harpoon',
+    },
     { action = 'VimwikiIndex', name = 'Wiki Index', section = 'VimWiki' },
     { action = 'Telescope themes', name = 'Themes', section = 'Neovim Internals' },
     { action = 'Telescope command_history', name = 'Command history', section = 'Neovim Internals' },
