8fc8b5a64038bee09e5c1e20cd014f8e5e2feb6e
Victor Ionescu <dsign.victor@gmail.com>
2022-10-01 20:53:29 +0300

    improve indent-blankline ContextStart highlight

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -1,3 +1,12 @@
+local i, _ = string.find(vim.g.theme, '-NvChad')
+local line = ''
+if i then
+  local colors = require('base46').get_theme_tb('base_30')
+  line = colors.dark_purple
+else
+  line = '#ff0000'
+end
+
 require('indent_blankline').setup({
   space_char_blankline = ' ',
   show_current_context = true,
@@ -6,3 +15,6 @@ require('indent_blankline').setup({
 })
 
 vim.cmd("let g:indent_blankline_filetype_exclude = ['starter', 'markdown', 'vimwiki']")
+
+vim.api.nvim_set_hl(0, 'IndentBlanklineContextStart', {})
+vim.api.nvim_set_hl(0, 'IndentBlanklineContextStart', { underline = true, sp = line })
