da09240d8e0fa3b945e21522612dd9fd13f27743
Victor Ionescu <dsign.victor@gmail.com>
2022-10-03 13:53:42 +0300

    show LSP diag on hover

--- lua/config/lsp/helpers.lua
+++ lua/config/lsp/helpers.lua
@@ -12,6 +12,20 @@ end
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 M.on_attach = function(client, bufnr)
+  vim.api.nvim_create_autocmd('CursorHold', {
+    buffer = bufnr,
+    callback = function()
+      local opts = {
+        focusable = false,
+        close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
+        border = 'rounded',
+        source = 'always',
+        prefix = ' ',
+        scope = 'cursor',
+      }
+      vim.diagnostic.open_float(nil, opts)
+    end,
+  })
   if client.supports_method('textDocument/formatting') then
     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
     vim.api.nvim_create_autocmd('BufWritePre', {
