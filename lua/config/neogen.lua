2e872d82cabe5ce55bbd26c895ab2764d2f01b5d
Victor Ionescu <dsign.victor@gmail.com>
2022-10-04 12:36:55 +0300

    change doc keymap

--- lua/config/neogen.lua
+++ lua/config/neogen.lua
@@ -1,5 +1,5 @@
 require('neogen').setup({ snippet_engine = 'luasnip' })
 
-vim.keymap.set('n', '<leader>d', function()
+vim.keymap.set('n', '<leader>dd', function()
   pcall(require('neogen').generate)
 end, { desc = 'Generate function documentation' })
