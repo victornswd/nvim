ba73e98acd87f4c19e459522770124c27e5be898
Victor Ionescu <dsign.victor@gmail.com>
2023-01-18 11:51:23 +0200

    improve mini.ai config

--- lua/config/mini.lua
+++ lua/config/mini.lua
@@ -1,5 +1,15 @@
 vim.defer_fn(function()
-	require("mini.ai").setup({})
+	local ai = require("mini.ai")
+	ai.setup({
+		custom_textobjects = {
+			o = ai.gen_spec.treesitter({
+				a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+				i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+			}, {}),
+			f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+			c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+		},
+	})
 	require("mini.align").setup({})
 	require("mini.pairs").setup()
 	require("mini.tabline").setup({})
