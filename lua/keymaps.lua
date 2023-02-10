b441294182187d0d1dd77d81f97e0c3ccfb0b5d4
Victor Ionescu <dsign.victor@gmail.com>
2023-02-10 17:14:34 +0200

    added right click context menu with LSP commands

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -71,9 +71,6 @@ vim.keymap.set("n", "<leader>s", ":mksession<CR>", { desc = "Save current files 
 vim.keymap.set("i", "<C-c>", EscapePair, { desc = "Escape pairs while in insert mode" })
 vim.keymap.set("i", "<C-w>", EscapePair, { desc = "Escape pairs while in insert mode" })
 
--- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move down selected line" })
--- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move up selected line" })
-
 function HL_search()
 	local ns = vim.api.nvim_create_namespace("search")
 	vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
@@ -86,3 +83,20 @@ function HL_search()
 end
 vim.keymap.set("n", "n", "nzz:lua HL_search()<CR>", { desc = "Inline search occurence" })
 vim.keymap.set("n", "N", "Nzz:lua HL_search()<CR>", { desc = "Inline search occurence" })
+
+-- Mouse context menu
+vim.api.nvim_create_autocmd({ "LspAttach" }, {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client.server_capabilities.hoverProvider then
+			vim.cmd.aunmenu({ "PopUp" })
+			vim.cmd.anoremenu({ "PopUp.Go\\ to\\ Definition <Cmd>lua vim.lsp.buf.definition()<CR>" })
+			vim.cmd.anoremenu({
+				"PopUp.Go\\ to\\ References <Cmd>lua pcall(require('telescope.builtin').lsp_references)<CR>",
+			})
+			vim.cmd.anoremenu({ "PopUp.Rename\\ Symbol <Cmd>lua vim.lsp.buf.rename()<CR>" })
+		end
+	end,
+})
+
+vim.keymap.set("n", "<leader><leader>", ":popup PopUp<CR>", { desc = "Show context menu" })
