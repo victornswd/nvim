337cecbdf081c26348740ef43407dc96d49d668c
Victor Ionescu <dsign.victor@gmail.com>
2023-03-02 16:31:17 +0200

    added treesitter performance trick from mars90226

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -4,6 +4,56 @@
 -- 	require("base46").load_highlight("treesitter")
 -- end
 
+local nvim_treesitter = {}
+
+nvim_treesitter.enable_config = {
+	highlight_definitions = false,
+	highlight_current_scope = false,
+}
+
+local get_buffer_variable = function(buf, var)
+	local status, result = pcall(vim.api.nvim_buf_get_var, buf, var)
+	if status then
+		return result
+	end
+	return nil
+end
+
+local force_disable_var = "nvim_treesitter_force_disable"
+local get_force_disable = function(bufnr)
+	return get_buffer_variable(bufnr, force_disable_var) or false
+end
+
+-- Disable check for highlight, highlight usage, highlight context module
+local disable_check = function(type, lang, bufnr)
+	if get_force_disable(bufnr) then
+		return true
+	end
+	if type == nil then
+		type = "base"
+	end
+
+	local line_count = vim.api.nvim_buf_line_count(bufnr or 0)
+	local line_threshold_map = vim.F.if_nil(nvim_treesitter.line_threshold[type], {})
+	local line_threshold = line_threshold_map[lang]
+
+	if line_threshold ~= nil and line_count > line_threshold then
+		return true
+	else
+		return false
+	end
+end
+
+-- Disable check for highlight module
+local base_disable_check = function(lang, bufnr)
+	return disable_check("base", lang, bufnr)
+end
+
+-- Disable check for highlight usage/context
+local extension_disable_check = function(lang, bufnr)
+	return disable_check("extension", lang, bufnr)
+end
+
 require("nvim-treesitter.install").compilers = { "gcc" }
 local ts = require("nvim-treesitter.configs")
 ts.setup({
@@ -40,6 +90,17 @@ ts.setup({
 	},
 	indent = {
 		enable = false,
+	},
+	refactor = {
+		highlight_definitions = {
+			enable = nvim_treesitter.enable_config.highlight_definitions,
+			disable = base_disable_check,
+			clear_on_cursor_move = false,
+		},
+		highlight_current_scope = {
+			enable = nvim_treesitter.enable_config.highlight_current_scope,
+			disable = extension_disable_check,
+		},
 	},
 	-- context_commentstring = {
 	-- 	enable = true,
@@ -120,3 +181,113 @@ ts.setup({
 		enable = true,
 	},
 })
+
+local check_condition = function(plugin_spec, condition)
+	if condition then
+		return plugin_spec
+	else
+		return nil
+	end
+end
+
+local setup_performance_trick = function()
+	local configs_commands = require("nvim-treesitter.configs").commands
+
+	-- TODO: Check if these actually help performance, initial test reveals that these may reduce highlighter time, but increase "[string]:0" time which is probably the time spent on autocmd & syntax enable/disable.
+	-- TODO: These config help reduce memory usage, see if there's other way to fix high memory usage.
+	-- TODO: Change to tab based toggling
+	local augroup_id = vim.api.nvim_create_augroup("nvim_treesitter_settings", {})
+
+	local global_idle_disabled_modules = vim.tbl_filter(function(module)
+		return module ~= nil
+	end, {
+		"highlight",
+		"context_commentstring",
+		"matchup",
+		check_condition("highlight_current_scope", nvim_treesitter.enable_config.highlight_current_scope),
+		check_condition("highlight_definitions", nvim_treesitter.enable_config.highlight_definitions),
+		"navigation",
+		"smart_rename",
+	})
+	local tab_idle_disabled_modules = global_idle_disabled_modules
+
+	local global_trick_delay_enable = false
+	local global_trick_delay = 60 * 1000 -- 60 seconds
+	vim.api.nvim_create_autocmd({ "FocusGained" }, {
+		group = augroup_id,
+		pattern = "*",
+		callback = function()
+			if global_trick_delay_enable then
+				global_trick_delay_enable = false
+			else
+				for _, module in ipairs(global_idle_disabled_modules) do
+					configs_commands.TSEnable.run(module)
+				end
+			end
+		end,
+	})
+	-- NOTE: We want to disable highlight if FocusLost is caused by following reasons:
+	-- 1. neovim goes to background
+	-- 2. tmux switch window, client
+	-- 3. Terminal emulator switch tab
+	-- We don't want to disable highlight if FocusLost is caused by following reasons:
+	-- 1. tmux switch pane
+	-- 2. Terminal emulator switch pane
+	-- 3. OS switch application
+	-- In other words, we want treesitter highlight if the buffer is actually displayed on the screen.
+	vim.api.nvim_create_autocmd({ "FocusLost" }, {
+		group = augroup_id,
+		pattern = "*",
+		callback = function()
+			global_trick_delay_enable = true
+
+			vim.defer_fn(function()
+				if global_trick_delay_enable then
+					for _, module in ipairs(global_idle_disabled_modules) do
+						configs_commands.TSDisable.run(module)
+					end
+
+					global_trick_delay_enable = false
+				end
+			end, global_trick_delay)
+		end,
+	})
+	local tab_trick_enable = false
+	local tab_trick_debounce = 200
+	-- FIXME: Open buffer in other tab doesn't have highlight
+	-- FIXME: Seems to conflict with true-zen.nvim
+	vim.api.nvim_create_autocmd({ "TabEnter" }, {
+		group = augroup_id,
+		pattern = "*",
+		callback = function()
+			tab_trick_enable = true
+
+			vim.defer_fn(function()
+				if tab_trick_enable then
+					local winids = vim.api.nvim_tabpage_list_wins(0)
+
+					for _, module in ipairs(tab_idle_disabled_modules) do
+						for _, winid in ipairs(winids) do
+							configs_commands.TSBufEnable.run(module, vim.api.nvim_win_get_buf(winid))
+						end
+					end
+
+					tab_trick_enable = false
+				end
+			end, tab_trick_debounce)
+		end,
+	})
+	vim.api.nvim_create_autocmd({ "TabLeave" }, {
+		group = augroup_id,
+		pattern = "*",
+		callback = function()
+			for _, winid in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+				for _, module in ipairs(tab_idle_disabled_modules) do
+					configs_commands.TSBufDisable.run(module, vim.api.nvim_win_get_buf(winid))
+				end
+			end
+		end,
+	})
+end
+
+setup_performance_trick()
