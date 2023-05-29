3c34514c76fab8d067c8802b5501d54d52ec3cb4
Victor Ionescu <dsign.victor@gmail.com>
2023-05-29 13:32:29 +0300

    update ts-context-commentstring in mini.comment

--- lua/config/mini.lua
+++ lua/config/mini.lua
@@ -15,9 +15,9 @@ vim.defer_fn(function()
 	require("mini.pairs").setup()
 	require("mini.tabline").setup({})
 	require("mini.comment").setup({
-		hooks = {
-			pre = function()
-				require("ts_context_commentstring.internal").update_commentstring({})
+		options = {
+			custom_commentstring = function()
+				return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
 			end,
 		},
 	})
