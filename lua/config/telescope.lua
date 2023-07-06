bec2a6e7c513dd72722e18bc3a7b29d9d51a88a2
Victor Ionescu <dsign.victor@gmail.com>
2023-07-06 13:17:17 +0300

    telescope fzf plugin

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -53,8 +53,17 @@ telescope.setup({
 			enable_preview = true,
 		},
 	},
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case",
+		},
+	},
 })
 
+require("telescope").load_extension("fzf")
 require("telescope").load_extension("undo")
 
 -- keymap
