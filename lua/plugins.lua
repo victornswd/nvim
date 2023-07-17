bbd4f5d8c00843e882e4b101ac099f443331b32c
Victor Ionescu <dsign.victor@gmail.com>
2023-07-17 17:33:00 +0300

    move to a maintained rainbow ()

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -102,7 +102,35 @@ return {
 		"nvim-treesitter/playground",
 		cmd = { "TSPlaygroundToggle", "TSHighlightCapturesUnderCursor", "TSNodeUnderCursor" },
 	},
-	{ "mrjones2014/nvim-ts-rainbow", event = "VeryLazy" }, -- NOTE:
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		event = "VeryLazy",
+		config = function()
+			-- This module contains a number of default definitions
+			local rainbow_delimiters = require("rainbow-delimiters")
+
+			vim.g.rainbow_delimiters = {
+				strategy = {
+					[""] = rainbow_delimiters.strategy["global"],
+					commonlisp = rainbow_delimiters.strategy["local"],
+				},
+				query = {
+					[""] = "rainbow-delimiters",
+					lua = "rainbow-blocks",
+				},
+				highlight = {
+					"RainbowDelimiterRed",
+					"RainbowDelimiterYellow",
+					"RainbowDelimiterBlue",
+					"RainbowDelimiterOrange",
+					"RainbowDelimiterGreen",
+					"RainbowDelimiterViolet",
+					"RainbowDelimiterCyan",
+				},
+				blacklist = { "c", "cpp" },
+			}
+		end,
+	}, -- NOTE:
 	{
 		"andymass/vim-matchup",
 		config = function()
