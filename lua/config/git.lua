1c94d310bbfafc3169c419f10f965e4c4da8b289
Victor Ionescu <dsign.victor@gmail.com>
2024-01-12 20:31:43 +0200

    move git related plugins into separate file

---
+++ lua/config/git.lua
@@ -0,0 +1,85 @@
+return {
+	{
+		"luukvbaal/statuscol.nvim",
+		opts = { setopt = true },
+		config = true,
+		branch = "0.10",
+		event = "VeryLazy",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					-- Navigation
+					map("n", "]c", function()
+						if vim.wo.diff then
+							return "]c"
+						end
+						vim.schedule(function()
+							gs.next_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
+
+					map("n", "[c", function()
+						if vim.wo.diff then
+							return "[c"
+						end
+						vim.schedule(function()
+							gs.prev_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
+
+					-- Actions
+					map("n", "<leader>hs", gs.stage_hunk)
+					map("n", "<leader>hr", gs.reset_hunk)
+					map("v", "<leader>hs", function()
+						gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+					map("v", "<leader>hr", function()
+						gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+					map("n", "<leader>hS", gs.stage_buffer)
+					map("n", "<leader>hu", gs.undo_stage_hunk)
+					map("n", "<leader>hR", gs.reset_buffer)
+					map("n", "<leader>hp", gs.preview_hunk)
+					map("n", "<leader>hb", function()
+						gs.blame_line({ full = true })
+					end)
+					map("n", "<leader>tb", gs.toggle_current_line_blame)
+					map("n", "<leader>hd", gs.diffthis)
+					map("n", "<leader>hD", function()
+						gs.diffthis("~")
+					end)
+					map("n", "<leader>td", gs.toggle_deleted)
+
+					-- Text object
+					map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+				end,
+			})
+		end,
+		event = "VeryLazy",
+	},
+	{
+		"akinsho/git-conflict.nvim",
+		version = "*",
+		opts = {
+			highlights = {
+				incoming = "ConflictDiffAdd",
+				current = "ConflictDiffText",
+			},
+		},
+		event = "VeryLazy",
+	},
+}
